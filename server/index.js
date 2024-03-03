49bcc3ca03155a2683f1e2fddf2b7d08dbebfdcc
Sergiy <sergiy0378@gmail.com>
2024-03-03 16:13:16 +0200

    feat: add error handler

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-
 import { initApi } from './src/api/api.js';
+import { errorHandler } from './src/middleware/error-handler/error-handler.middleware.js';
 
 const app = express();
 
 app.use(initApi(express.Router));
+app.use(errorHandler);
 
 app.listen(8080, () => console.log('Server listening to port 8080'));
