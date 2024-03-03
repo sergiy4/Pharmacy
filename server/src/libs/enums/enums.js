9c07258b05235bcdef02eae048728d2693499c0e
Sergiy <sergiy0378@gmail.com>
2024-03-03 18:19:49 +0200

    feat: + http_code enum

--- server/src/libs/enums/enums.js
+++ server/src/libs/enums/enums.js
@@ -2,3 +2,5 @@ export { AppRoute } from './app-route.enum.js';
 export { ApiPath } from './api-path.enum.js';
 export { TableName } from './table_name.enum.js';
 export { ColumnName } from './column_name.js';
+export { HttpCode } from './http-code.js';
+export { ErrorMessage } from './error-message.enum.js';
