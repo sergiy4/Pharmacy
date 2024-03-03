23f7e15d631f6560a89467871036c6d98b44b988
Sergiy <sergiy0378@gmail.com>
2024-03-03 20:19:37 +0200

    fix: shopApi

--- client/src/packages/shop/shop.ts
+++ client/src/packages/shop/shop.ts
@@ -3,11 +3,11 @@ import { ENV } from '../../libs/enums/enums';
 import { httpApi } from '../http/http';
 import { Shop as ShopApi } from './shop-api';
 
-const medicineApi = new ShopApi({
+const shopApi = new ShopApi({
   httpApi,
   apiPath: ENV.API_PATH,
 });
 
-export { medicineApi };
+export { shopApi };
 export { type GetShopsDtoResponse, type Shop };
 export { type ShopApi } from './libs/types/types';
