e9d13118cbc04bc406dd10b54d2e86791afd8ff5
Sergiy <sergiy0378@gmail.com>
2024-03-03 15:57:40 +0200

    feat: + services to api

--- server/src/api/api.js
+++ server/src/api/api.js
@@ -1,13 +1,14 @@
 import { AppRoute } from '../libs/enums/enums.js';
 import { initMedicine } from './medicines/medicines.api.js';
 import { initStore } from './store/store.api.js';
+import { medicineService, storeService } from '../services/index.js';
 
 const initApi = (Router) => {
   const apiRouter = Router();
 
-  apiRouter.use(AppRoute.MEDICINES, initMedicine(Router, {}));
+  apiRouter.use(AppRoute.MEDICINES, initMedicine(Router, { medicineService }));
 
-  apiRouter.use(AppRoute.STORES, initStore(Router, {}));
+  apiRouter.use(AppRoute.STORES, initStore(Router, { storeService }));
 
   return apiRouter;
 };
