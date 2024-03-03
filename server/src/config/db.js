e97ccb572d5df0d81f01d673ecfdadbbdbfa0bb3
Sergiy <sergiy0378@gmail.com>
2024-03-03 13:18:47 +0200

    feat: add db config

---
+++ server/src/config/db.js
@@ -0,0 +1,41 @@
+import { getENV } from './lib/env.enum.js';
+
+function initialConfig() {
+  const {
+    DATABASE: database,
+    HOST: host,
+    PASSWORD: password,
+    PORT: port,
+    USER: user,
+  } = getENV().DB;
+
+  console.log(database, host);
+  return {
+    development: {
+      client: 'postgresql',
+      connection: {
+        port,
+        host,
+        database,
+        user,
+        password,
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+      },
+    },
+  };
+}
+
+function environmentsConfig() {
+  return {
+    ['development']: initialConfig,
+    ['production']: initialConfig,
+  };
+}
+
+export { environmentsConfig };
