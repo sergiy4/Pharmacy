1fa2ef85598c88351d6c807d16813bdd33062225
Sergiy <sergiy0378@gmail.com>
2024-03-03 18:22:33 +0200

    feat: + store api

--- server/src/api/store/store.api.js
+++ server/src/api/store/store.api.js
@@ -1,9 +1,20 @@
 import { ApiPath } from '../../libs/enums/enums.js';
 
 const initStore = (Router, services) => {
+  const { storeService } = services;
   const router = Router();
 
-  router.get(ApiPath.ROOT, async (req, res, next) => {});
+  router.get(ApiPath.ROOT, async (req, res, next) => {
+    try {
+      const stores = await storeService.getStores();
+
+      return res.send({
+        stores,
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 
   return router;
 };
