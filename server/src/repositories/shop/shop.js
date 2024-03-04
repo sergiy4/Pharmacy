5ef6adaaf2d39c3fa8cb35a8454d1e0c10a2832b
Sergiy <sergiy0378@gmail.com>
2024-03-04 09:55:44 +0200

    fix: fix name

--- server/src/repositories/shop/shop.js
+++ server/src/repositories/shop/shop.js
@@ -2,7 +2,7 @@ import { TableName } from '../../libs/enums/enums.js';
 import { Shop as ShopRepository } from './shop.repository.js';
 
 const shopRepository = new ShopRepository({
-  tableName: TableName.shopS,
+  tableName: TableName.SHOPS,
 });
 
 export { shopRepository };
