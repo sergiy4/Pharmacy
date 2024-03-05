ab2ddb978f2c4bd798f2959e488ae46706a00151
Sergiy <sergiy0378@gmail.com>
2024-03-05 09:03:33 +0200

    feat: add component export

--- client/src/libs/components/components.ts
+++ client/src/libs/components/components.ts
@@ -1,2 +1,5 @@
 export { Layout } from './layout/layout';
 export { ErrorPage } from './error-page/error-page';
+export { Button } from './button/button';
+export { Input } from './input/input';
+export { Loader } from './loader/loader';
