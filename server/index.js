2fc7953084ad20e76141a6aa9639b65c40bb8d67
Sergiy <sergiy0378@gmail.com>
2024-03-05 15:29:47 +0200

    fix: fix index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { errorHandler } from './src/middleware/error-handler/error-handler.middl
 import cors from 'cors';
 
 const app = express();
+const PORT = process.env.PORT || '4000';
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,4 @@ app.use(express.json());
 app.use(initApi(express.Router));
 app.use(errorHandler);
 
-app.listen(process.env.PORT || '4000', () =>
-  console.log('Server listening to port 8080')
-);
+app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
